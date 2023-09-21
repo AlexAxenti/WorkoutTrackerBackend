1a2c945535f9d2b4580d7e09cb7013ee37fd5714
AlexAxenti <alex.axenti@gmail.com>
2023-09-21 15:19:31 -0400

    Created update endpoints

--- server/controllers/logs.js
+++ server/controllers/logs.js
@@ -10,6 +10,19 @@ function postLogs (req, res) {
     res.send('Post Logs')
 };
 
+function updateLogs (req, res) {
+    let reqBody = req.body
+    let logId = reqBody._id
+
+    Log.findOne({ _id: logId }).then(log => {
+        log.logName = reqBody.logName
+        log.logRoutine = reqBody.logRoutine
+        log.logExercises = reqBody.logExercises
+
+        log.save()
+    })
+}
+
 module.exports = {
     getLogs,
     postLogs,
--- server/controllers/routines.js
+++ server/controllers/routines.js
@@ -10,6 +10,18 @@ function postRoutines (req, res) {
     res.send('Post Routines')
 };
 
+function updateRoutines (req, res) {
+    let reqBody = req.body
+    let routineId = reqBody._id
+
+    Routine.findOne({ _id: routineId }).then(routine => {
+        routine.routineName = reqBody.routineName
+        routine.exerciseNames = reqBody.exerciseNames
+
+        routine.save()
+    });
+};
+
 module.exports = {
     getRoutines,
     postRoutines,
