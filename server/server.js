d1fa6ea3ea90c1ebf0bb2310ffca292c12541e24
AlexAxenti <alex.axenti@gmail.com>
2023-09-25 15:13:13 -0400

    Added missing cors package

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose');
-const bodyparser = require("body-parser");
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const port = 7000
